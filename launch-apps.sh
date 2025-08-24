e3009c823bfe45450b2ec5d4f095aaaee9105b0f
stephen <stephenbmulligan@gmail.com>
2025-08-24 12:27:02 +0200

    fixed disown statements, added some echos for each process

--- launch-apps.sh
+++ launch-apps.sh
@@ -7,15 +7,22 @@ if [[ "$1" == "--bt" ]]; then
 fi
 
 # Run agenda construction.sh with a timeout of 30 seconds
+echo "constructing agenda..."
 timeout 30s bash /home/stephen/local_workdir/personal/agenda_construction/agenda_construction.sh
 if [[ $? -eq 124 ]]; then
   echo "agenda_construction.sh timed out after 30 seconds."
 fi
 
 # set up ssh
+echo "ssh setup..."
 ssh-add /home/stephen/.ssh/id_rsa
 
+echo "pushing vault..."
 bash /home/stephen/local_workdir/personal/scripts/push-vault.sh
+echo "launching apps..."
 flatpak run org.mozilla.firefox &
+disown
 flatpak run md.obsidian.Obsidian &
+disown
 zotero &
+disown
